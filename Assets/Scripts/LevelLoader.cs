8dcad730d76c3a9ac632277edab40bc80d4cf7ab
ColinJWright <colinwright2771@gmail.com>
2021-02-24 18:14:44 +0000

    Make A Splash Screen Lesson complete

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] int timeToWait = 3;
+    int currentSceneIndex;
+
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(WaitForTime());
+        }
+    }
+
+    IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex = 1);
+    }
+}

bf3e758d05fc1ef469069e4915519a931fca1ab7
ColinJWright <colinwright2771@gmail.com>
2021-02-28 15:02:16 +0000

    Make Projectile Move Lesson complete

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [Range(0f, 10f)] [SerializeField] float projectileSpeed = 10f;
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * projectileSpeed * Time.deltaTime);
+    }
+}

86f7258f98b1668c842a96cc545fe35a915c8a1a
ColinJWright <colinwright2771@gmail.com>
2021-03-01 16:14:32 +0000

    Implementing Health & Damage Lesson complete

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [Range(0f, 10f)] [SerializeField] float projectileSpeed = 10f;
+    [SerializeField] float damage = 50f;
 
     void Update()
     {
         transform.Translate(Vector2.right * projectileSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        var health = otherCollider.GetComponent<Health>();
+        health.DealDamage(damage);
     }
 }

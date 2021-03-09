5ce2f6cb49d1d833f084bc5df3d8c543176de215
ColinJWright <colinwright2771@gmail.com>
2021-03-09 16:31:17 +0000

    Defenders Costing Resources Lesson complete

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -10,4 +10,9 @@ public class Defender : MonoBehaviour
     {
         FindObjectOfType<StarDisplay>().AddStars(amount);
     }
+
+    public int GetStarCost()
+    {
+        return starCost;
+    }
 }

9ab7f5515bfe8b10629bfb968f8f6a72ed5f300f
ColinJWright <colinwright2771@gmail.com>
2021-03-11 16:22:10 +0000

    Attacker In My Lane Lesson complete

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -6,6 +6,50 @@ public class Shooter : MonoBehaviour
 {
     [SerializeField] GameObject projectile;
     [SerializeField] GameObject gun;
+    AttackerSpawner myLaneSpawner;
+
+    private void Start()
+    {
+        SetLaneSpawner();
+    }
+    private void Update()
+    {
+        if (IsAttackerInLane())
+        {
+            Debug.Log("Shoot");
+        }
+        else
+        {
+            Debug.Log("Sit and Wait");
+        }
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach (AttackerSpawner spawner in spawners)
+        {
+            bool IsCloseEnough = (Mathf.Abs(spawner.transform.position.y - transform.position.y)<= Mathf.Epsilon);
+
+            if (IsCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+        if (myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
 
     public void Fire()
     {
